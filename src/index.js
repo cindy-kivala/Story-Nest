a1d22adb604fda6eb4a9faa4de85677fc7aa8405
cindy kivala <cindykivala@gmail.com>
2025-06-25 12:00:16 +0300

    Main js attempt 2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 //DOM content loaded
-document.addEventListener("DOMContentLoaded", () => {
+// document.addEventListener("DOMContentLoaded", () => {
   // Get the form element
-  const form = document.getElementById("searcher");
+//   const form = document.getElementById("searcher");
 
   // Add an event listener for the submit event
-  form.addEventListener("submit", (event) => {
+//   form.addEventListener("submit", (event) => {
     // Prevent the default form submission behavior
-    event.preventDefault();
+    // event.preventDefault();
 
     // Get the input value
-    const input = document.getElementById("#search-input").value;
+    // const input = document.getElementById("#search-input").value;
 
     // Log the input value to the console
-    console.log(input);
-  });
-});
+//     console.log(input);
+//   });
+// });
+document.addEventListener("DOMContentLoaded", main);
