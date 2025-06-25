2903f978c6effeb031df1952f50ffbcda44b18ec
cindy kivala <cindykivala@gmail.com>
2025-06-25 12:40:50 +0300

    submit listener

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ document.addEventListener("DOMContentLoaded", main);
 function main() {
     const form = document.getElementById("searcher");
     const input = document.getElementById("search-input");
+   //listener for form submission
+   form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+   })
 }
