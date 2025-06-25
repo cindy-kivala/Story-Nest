da1c28d1c81617eac39ab5e4d6674cadc59b18b2
cindy kivala <cindykivala@gmail.com>
2025-06-25 12:36:49 +0300

    function main declaration

--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
-//DOM content loaded
-// document.addEventListener("DOMContentLoaded", () => {
-  // Get the form element
-//   const form = document.getElementById("searcher");
-
-  // Add an event listener for the submit event
-//   form.addEventListener("submit", (event) => {
-    // Prevent the default form submission behavior
-    // event.preventDefault();
-
-    // Get the input value
-    // const input = document.getElementById("#search-input").value;
-
-    // Log the input value to the console
-//     console.log(input);
-//   });
-// });
-document.addEventListener("DOMContentLoaded", main => {
-    //function main()
-    function main() {
-        //load books on page load
-        fetchBooks();
-    }
-  
+document.addEventListener("DOMContentLoaded", main);
+//function main
+function main() {
+    const form = document.getElementById("searcher");
+    const input = document.getElementById("search-input");
+}
