4f296a64b6a91f0802dfe649f91405ed76cc30b7
cindy kivala <cindykivala@gmail.com>
2025-06-25 12:53:10 +0300

    input field conditionals

--- src/index.js
+++ src/index.js
@@ -5,7 +5,17 @@ function main() {
     const input = document.getElementById("search-input");
    //listener for form submission
    form.addEventListener("submit", (event) => {
-    event.preventDefault();
-    
+    event.preventDefault();// prevent page relod
+    // get the value of the input field
+    const searchTerm = input.value.trim();
+
+    // check if the input is empty
+    if (searchTerm === "") {
+        alert("Please enter a book title, author, ISBN, or genre before searching.");
+    } else {
+        // redirect to the search results page with the search term as a query parameter
+        fetchBooks(searchTerm); //call API function if input is not empty 
+    }
+
    })
 }
